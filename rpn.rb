a09d252acc2c543299b5014da1784b7124a088c0
Jordan Grogan <jordangrogan11@gmail.com>
2018-04-07 12:21:08 -0400

    Added RPN class

--- rpn.rb
+++ rpn.rb
@@ -1,12 +1,32 @@
-if ARGV.count > 0
+class RPN
 
+  def execute(line)
+    puts line
+  end
+
+end
+
+# Main Execution Begins Here
+
+rpn = RPN.new
+
+if ARGV.count > 0
   # File mode
+
   lines = []
   File.open(ARGV[0], "r").each_line do |line|
-    lines << line.chomp
-    puts line
+    lines << line.chomp.upcase
+    execute(line)
   end
 
 else
   # REPL mode
+
+  input = nil
+  while(input != "QUIT")
+    print "> "
+    input = gets().chomp.upcase
+    rpn.execute(input)
+  end
+
 end
