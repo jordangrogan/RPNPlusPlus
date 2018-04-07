b333f6fd3e8fadb705d372d6807da30199772548
Jordan Grogan <jordangrogan11@gmail.com>
2018-04-07 12:38:50 -0400

    Added LET operation

--- rpn.rb
+++ rpn.rb
@@ -1,9 +1,28 @@
 class RPN
 
-  def execute(line)
-    puts line
+  def initialize
+    @call_stack = []
+    @variables = {}
   end
 
+  def execute(line)
+
+    tokens = line.split (" ")
+
+    if(tokens.size > 0)
+
+      if(tokens[0] == "LET")
+        puts "assigning #{tokens[2]} to #{tokens[1]}"
+        @variables[tokens[1]] = tokens[2]
+      end
+
+      if(tokens[0] == "PRINT")
+        puts "printing"
+      end
+
+    end
+
+  end
 end
 
 # Main Execution Begins Here
@@ -16,7 +35,7 @@ if ARGV.count > 0
   lines = []
   File.open(ARGV[0], "r").each_line do |line|
     lines << line.chomp.upcase
-    execute(line)
+    rpn.execute(line)
   end
 
 else
