b242fc7304c180e5b52aa4b495f6168dd5012c87
Jordan Grogan <jordangrogan11@gmail.com>
2018-04-07 12:55:42 -0400

    Add variables

--- rpn.rb
+++ rpn.rb
@@ -12,17 +12,30 @@ class RPN
     if(tokens.size > 0)
 
       if(tokens[0] == "LET")
-        puts "assigning #{tokens[2]} to #{tokens[1]}"
         @variables[tokens[1]] = tokens[2]
       end
 
       if(tokens[0] == "PRINT")
-        puts "printing"
+        print_op(tokens[1,tokens.length])
       end
 
     end
 
   end
+
+  def print_op(arr)
+    if(arr.size == 1)
+      print arr
+    else
+      arr.each do |token|
+        @call_stack.push(token)
+      end
+      if(@call_stack.pop == "+")
+        puts @variables[@call_stack.pop].to_i + @variables[@call_stack.pop].to_i
+      end
+    end
+  end
+
 end
 
 # Main Execution Begins Here
