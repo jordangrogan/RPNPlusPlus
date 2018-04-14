362c7a16fa2af192e23f353e09d00a26f4c13d32
Jordan Grogan <jordangrogan11@gmail.com>
2018-04-13 22:10:43 -0400

    Added a few unit tests to rpn_executor

--- rpn_executor_test.rb
+++ rpn_executor_test.rb
@@ -1,4 +1,5 @@
 require 'minitest/autorun'
+require 'rantly/minitest_extensions'
 
 require_relative 'rpn_executor'
 
@@ -8,8 +9,51 @@ class RPNExecutorTest < Minitest::Test
     @rpn = RPNExecutor.new
   end
 
-  def test_adding
-    assert_equal 7, @rpn.execute("4 3 +")
+  # System Level Testing
+  # def test_adding
+  #   assert_equal 7, @rpn.execute("4 3 +")
+  # end
+
+  # UNIT TESTS FOR is_operator? FUNCTION
+
+  # Test that it returns true when +, -, /, are * are input
+  def test_is_operator
+    assert @rpn.is_operator?('+')
+    assert @rpn.is_operator?('-')
+    assert @rpn.is_operator?('/')
+    assert @rpn.is_operator?('*')
+  end
+
+  # Test that it returns false when +, -, /, are * are not an input
+  def test_is_operator_not_an_operator
+    refute @rpn.is_operator?('1')
+    refute @rpn.is_operator?('a')
+  end
+
+  # UNIT TESTS FOR CALCULATE
+
+  # Test a sample operation
+  def test_calculate
+    tokens = "10 10 * 5 5 * 0 0 * + +".split(' ')
+    assert @rpn.calculate(tokens), 125
+  end
+
+  # Use property based testing to ensure calculations are correct
+  def test_calculate_property_based
+    property_of {
+      x = integer
+      y = integer
+      [x, y]
+    }.check { |x, y|
+      sum = x + y
+      diff = y - x
+      prod = x * y
+      quot = y/x
+      assert @rpn.calculate([x.to_s, y.to_s, '+']), sum
+      assert @rpn.calculate([x.to_s, y.to_s, '-']), diff
+      assert @rpn.calculate([x.to_s, y.to_s, '*']), prod
+      assert @rpn.calculate([x.to_s, y.to_s, '/']), quot
+    }
   end
 
 end
