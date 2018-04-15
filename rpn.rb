2c2f79808483aa2815fac613dc23e8454e543c45
Jordan Grogan <jordangrogan11@gmail.com>
2018-04-14 20:03:20 -0400

    Fixed rubocop issues on rpn.rb

--- rpn.rb
+++ rpn.rb
@@ -1,39 +1,34 @@
 require_relative 'rpn_executor'
 
-trap "SIGINT" do exit end # Handle quitting with control-C gracefully
-
 rpn = RPNExecutor.new
 
 if ARGV.count > 0
   # File mode
 
-    value = nil
-    ARGV.each do |file|
-      File.open(file, 'r').each_line do |line|
-        value = rpn.execute(line.upcase)
-        if(value).is_a?(Error)
-          puts "Line #{i}: #{value.error_message}"
-        else
-          value
-        end
-        break if value == "QUIT"
+  value = nil
+  ARGV.each do |file|
+    File.open(file, 'r').each_line do |line|
+      value = rpn.execute(line.upcase)
+      if value.is_a?(Error)
+        puts "Line #{i}: #{value.error_message}"
+      else
+        value
       end
-      break if value == "QUIT"
+      break if value == 'QUIT'
     end
+    break if value == 'QUIT'
+  end
 
 else
   # REPL mode
 
-  value = nil
   line_count = 0
-  while(value != "QUIT")
+  while value != 'QUIT'
     line_count += 1
     print '> '
     value = rpn.execute(gets.chomp.upcase)
-    if(value).is_a?(Error)
-      puts "Line #{line_count}: #{value.error_message}"
-    end
-    puts value if value != "QUIT" && !value.is_a?(Error)
+    puts "Line #{line_count}: #{value.error_message}" if value.is_a?(Error)
+    puts value if value != 'QUIT' && !value.is_a?(Error)
   end
 
 end
